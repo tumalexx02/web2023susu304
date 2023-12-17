70d43a544642bfa9fca26f411863951ac8270201
Aleksandr Tumanov <tumalexx02@gmail.com>
2023-12-17 21:55:45 +0500

    Finished POSTS LIST

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const loader = document.getElementById('loader');
 const modal = document.getElementById('modal');
 const modalCloseButton = document.getElementById('close-btn');
 const addButton = document.getElementById('create-post');
+const form = document.querySelector('form');
 
 let emptyPost;
 let users;
@@ -12,7 +13,7 @@ let posts;
 const getPost = (post, users) => {
   const newPost = document.createElement('div');
   newPost.classList =
-    'container px-4 py-4 bg-gray-800 rounded-md border-2 border-gray-200 justify-self-center w-[90%] md:w-full';
+    `container px-4 py-4 bg-gray-800 rounded-md border-2 border-gray-200 justify-self-center w-[90%] md:w-full card ${localStorage.getItem('theme') === 'dark' ? 'dark' : 'light'}`;
   newPost.setAttribute('key', post.id);
   newPost.setAttribute('userId', post.userId);
 
@@ -91,32 +92,38 @@ const addPostsToDOM = async () => {
 
   loader.classList.add('hidden');
 
-  emptyPost = getPost({
-    id: users.length + 1,
-    title: '',
-    body: '',
-    userId: 1
-  }, users)
+  emptyPost = getPost(
+    {
+      id: users.length + 1,
+      title: '',
+      body: '',
+      userId: 1,
+    },
+    users
+  );
 };
 
 function handleClick(e) {
   if (e.target.parentNode.classList.contains('edit-button')) {
     const post = e.target.parentNode.parentNode.parentNode.parentNode;
-    showModal(post)
+    showModal(post);
   } else if (e.target.parentNode.classList.contains('delete-button')) {
     const post = e.target.parentNode.parentNode.parentNode.parentNode;
     deletePost(post);
   }
 }
 
-function showModal(post) {
-  const title = document.querySelector('#post-title') 
+function showModal(post, key) {
+  const title = document.querySelector('#post-title');
   title.value = post.querySelector('h2').innerText;
-  const body = document.querySelector('#post-body')
+  const body = document.querySelector('#post-body');
   body.value = post.querySelector('p').innerText;
-  const select = document.getElementById('post-user')
+  const select = document.getElementById('post-user');
   select.selectedIndex = post.getAttribute('userId') - 1;
   modal.classList.replace('hidden', 'block');
+  document
+    .querySelector('form')
+    .setAttribute('data-post-id', post ? post.getAttribute('key') : '');
 }
 
 function deletePost(post) {
@@ -128,37 +135,81 @@ function deletePost(post) {
   ).then(() => postsList.removeChild(post));
 }
 
-function createPost() {
-  const post = {
-    title: modal.getElementById('post-title').value,
-    body: modal.getElementById('post-body').value,
-  }
-
-  fetch(`https://jsonplaceholder.typicode.com/posts`,
-  {
-    method: 'POST',
-    body: JSON.stringify({
-
-    })
-  })
-}
-
 function closeModal(e) {
-  e.preventDefault()
-  modal.classList.replace('block', 'hidden')
-  const title = document.querySelector('#post-title') 
+  e.preventDefault();
+  modal.classList.replace('block', 'hidden');
+  const title = document.querySelector('#post-title');
   title.value = '';
-  const body = document.querySelector('#post-body')
+  const body = document.querySelector('#post-body');
   body.value = '';
-  const select = document.getElementById('post-user')
+  const select = document.getElementById('post-user');
   select.selectedIndex = 0;
 }
 
+function handleSubmit(e) {
+  e.preventDefault();
+  // Получение данных из формы
+  const title = document.getElementById('post-title').value;
+  const body = document.getElementById('post-body').value;
+  const userId = selectUsers.value;
+  const postId = e.target.getAttribute('data-post-id');
+
+  const postData = { title, body, userId: Number(userId) };
+
+  const method = postId ? 'PUT' : 'POST';
+  const url = `https://jsonplaceholder.typicode.com/posts/${
+    postId ? postId : ''
+  }`;
+
+  fetch(url, {
+    method: method,
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+    body: JSON.stringify(postData),
+  })
+    .then((response) => response.json())
+    .then((post) => {
+      if (postId) {
+        // Это редактирование существующего поста
+        const oldPostElement = document.querySelector(`[key='${postId}']`);
+        const newPostElement = getPost(post, users);
+        console.log(oldPostElement);
+        oldPostElement.replaceWith(newPostElement);
+      } else {
+        const newPostElement = getPost(post, users);
+        postsList.appendChild(newPostElement);
+      }
+      closeModal(e);
+    })
+    .catch((error) => console.error('Error:', error));
+}
+
+function toggleTheme() {
+  const currentTheme = localStorage.getItem('theme') || 'light';
+  const newTheme = currentTheme === 'light' ? 'dark' : 'light';
+
+  document.body.classList.remove(currentTheme);
+  document.body.classList.add(newTheme);
+  document.querySelectorAll('.card').forEach(card => card.classList.remove(currentTheme));
+  document.querySelectorAll('.card').forEach(card => card.classList.add(newTheme));
+  localStorage.setItem('theme', newTheme);
+}
+
 function init() {
-  document.addEventListener('DOMContentLoaded', addPostsToDOM);
+  document.addEventListener('DOMContentLoaded', () => {
+    const savedTheme = localStorage.getItem('theme') || 'dark';
+    document.body.classList.add(savedTheme);
+    document.querySelectorAll('.card').forEach(card => card.classList.add(savedTheme));
+    document
+      .getElementById('theme-toggle')
+      .addEventListener('click', toggleTheme);
+    addPostsToDOM();
+  });
   postsList.addEventListener('click', handleClick);
-  addButton.addEventListener('click', () => showModal(emptyPost))
+  addButton.addEventListener('click', () => showModal(emptyPost));
   modalCloseButton.addEventListener('click', closeModal);
+  form.addEventListener('submit', handleSubmit);
 }
 
 init();
